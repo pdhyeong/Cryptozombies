c08638ba8767bcc0509f1e8eb85be1c506ada3b3
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2024-03-07 01:06:21 +0900

    Create ownable.sol

---
+++ ownable.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract Ownable {
+  address private _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  constructo{
+    _owner = msg.sender;
+    emit OwnershipTransferred(address(0), _owner);
+    }
+
+    modifier onlyOwner() {
+      require(isOwner());
+      _;
+    }
+
+    function isOwner() public view returns(bool) {
+      return msg.sender == _owner;
+    }
+
+    function trasnferOwnership(address newOwner) public onlyOwner {
+      _transferOwnership(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal {
+      require(newOwner != address(0));
+      emit OnwershipTransferred(_owner, newOwner);
+      _owner = newOwner;
+    }
+}
